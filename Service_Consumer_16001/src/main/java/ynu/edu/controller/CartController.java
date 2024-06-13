139f9232df5f31f503d1ce09440a72944114bb4c
lentee <yuxinyang2003@126.com>
2024-06-13 14:34:43 +0800

    first

---
+++ Service_Consumer_16001/src/main/java/ynu/edu/controller/CartController.java
@@ -0,0 +1,36 @@
+package ynu.edu.controller;
+
+import ynu.edu.entity.CommonResult;
+import ynu.edu.entity.User;
+import jakarta.annotation.Resource;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/getCartById/{userId}")
+    public CommonResult<User> getCartById(@PathVariable("userId") Integer userId) {
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("provider-server");
+        for (ServiceInstance instance : instanceList) {
+            System.out.println(instance.getHost() + "\t" + instance.getPort());
+        }
+        ServiceInstance instance = instanceList.get(0);
+        CommonResult<User> result = restTemplate.getForObject(
+                "http://" + instance.getHost() + ":" + instance.getPort() + "/user/getUserById/" + userId.toString(), CommonResult.class);
+        return result;
+    }
+}
---
+++ Service_Provider_11000/src/main/java/ynu/edu/controller/UserController.java
@@ -0,0 +1,32 @@
+package ynu.edu.controller;
+
+import ynu.edu.entity.CommonResult;
+import ynu.edu.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getUserById/{userId}")
+    public CommonResult<User> getUserById(@PathVariable("userId") Integer userId) {
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(服务由11000提供)";
+        try {
+            User u = new User(userId, "小明", "123456");
+            result.setResult(u);
+
+        } catch (Exception e) {
+            code = 500;
+            message = "failed";
+        }
+
+        result.setMessage(message);
+        result.setCode(code);
+        return result;
+    }
+
+}
